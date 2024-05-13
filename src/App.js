8748e8c7cdb6b90c52effa9301c56b674bb82ac1
Ankur <ankur.gupta.cd.eee19@itbhu.ac.in>
2024-05-13 15:11:05 +0530

    upload project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css'
+import DropdownBar from './components/dropdownRow/DropdownRow'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className={styles.container}>
+        <h className={styles.heading}>Let's start making Dashboard</h>
+      </div>
+      <DropdownBar />
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/data/dorpdownData.js
@@ -0,0 +1,38 @@
+const data = {
+  Portfolio1: {
+    'Startegy1.1': {
+      'Model1.1.1': '',
+      'Model1.1.2': '',
+      'Model1.1.3': '',
+    },
+    'Startegy1.2': {
+      'Model1.2.1': '',
+      'Model1.2.2': '',
+      'Model1.2.3': '',
+    },
+    'Startegy1.3': {
+      'Model1.3.1': '',
+      'Model1.3.2': '',
+      'Model1.3.3': '',
+    },
+  },
+  Portfolio2: {
+    'Startegy2.1': {
+      'Model2.1.1': '',
+      'Model2.1.2': '',
+      'Model2.1.3': '',
+    },
+    'Startegy2.2': {
+      'Model2.2.1': '',
+      'Model2.2.2': '',
+      'Model2.2.3': '',
+    },
+    'Startegy2.3': {
+      'Model2.3.1': '',
+      'Model2.3.2': '',
+      'Model2.3.3': '',
+    },
+  },
+}
+
+export default data
